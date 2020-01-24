352dbcc99f2b6af0c6cb0a494f641e168bcb394d
youngkiu <youngkiu@gmail.com>
2020-01-24 23:42:42 +0900

    update the code from cuda to torch.device

--- pytorch_CAM/main.py
+++ pytorch_CAM/main.py
@@ -16,22 +16,21 @@ from update import *
 
 # functions
 CAM             = 1
-USE_CUDA        = 1
 RESUME          = 0
-PRETRAINED      = 0
+PRETRAINED      = True
 
 
 # hyperparameters
-BATCH_SIZE      = 32
+BATCH_SIZE      = 64
 IMG_SIZE        = 224
 LEARNING_RATE   = 1e-3
-EPOCH           = 100
+EPOCH           = 20
 
 
 # prepare data
 normalize = transforms.Normalize(
-        mean=[0.485, 0.456, 0.406],
-        std=[0.229, 0.224, 0.225]
+    mean=[0.485, 0.456, 0.406],
+    std=[0.229, 0.224, 0.225]
 )
 
 transform_train = transforms.Compose([
@@ -51,48 +50,50 @@ transform_test = transforms.Compose([
 # https://www.microsoft.com/en-us/download/confirmation.aspx?id=54765
 train_data = datasets.ImageFolder(
     '~/dataset/dataset_dogs_vs_cats/train/', transform=transform_train)
-trainloader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
+trainloader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
 
 test_data = datasets.ImageFolder(
     '~/dataset/dataset_dogs_vs_cats/test/', transform=transform_test)
-testloader = DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=False, num_workers=2)
+testloader = DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=False)
 
 
-# class
-classes = {0: 'cat', 1: 'dog'}
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Using device:', device)
 
 
 # fine tuning
 if PRETRAINED:
-    net = inception_v3(pretrained=PRETRAINED)
-    for param in net.parameters():
+    model = inception_v3(pretrained=PRETRAINED)
+    for param in model.parameters():
         param.requires_grad = False
-    net.fc = torch.nn.Linear(2048, 2)
+    model.fc = torch.nn.Linear(2048, 2)
 else:
-    net = inception_v3(pretrained=PRETRAINED, num_classes=len(classes))
-final_conv = 'Mixed_7c'
+    model = inception_v3(pretrained=PRETRAINED,
+                         num_classes=len(train_data.classes))
 
-net.cuda()
+model.to(device)
 
 
 # load checkpoint
 if RESUME != 0:
     print("===> Resuming from checkpoint.")
     assert os.path.isfile('checkpoint/'+ str(RESUME) + '.pt'), 'Error: no checkpoint found!'
-    net.load_state_dict(torch.load('checkpoint/' + str(RESUME) + '.pt'))
+    model.load_state_dict(torch.load('checkpoint/' + str(RESUME) + '.pt'))
 
 
 # retrain
-criterion = torch.nn.CrossEntropyLoss()
+criterion = torch.nn.CrossEntropyLoss().to(device)
 
 if PRETRAINED:
-    optimizer = torch.optim.SGD(net.fc.parameters(), lr=LEARNING_RATE, momentum=0.9, weight_decay=5e-4)
+    optimizer = torch.optim.SGD(
+        model.fc.parameters(), lr=LEARNING_RATE, momentum=0.9, weight_decay=5e-4)
 else:
-    optimizer = torch.optim.SGD(net.parameters(), lr=LEARNING_RATE, momentum=0.9, weight_decay=5e-4)
+    optimizer = torch.optim.SGD(
+        model.parameters(), lr=LEARNING_RATE, momentum=0.9, weight_decay=5e-4)
 
-for epoch in range (1, EPOCH + 1):
-    retrain(trainloader, net, USE_CUDA, epoch, criterion, optimizer)
-    retest(testloader, net, USE_CUDA, criterion, epoch, RESUME)
+for epoch in range(EPOCH):
+    retrain(trainloader, model, device, epoch, criterion, optimizer)
+    retest(testloader, model, device, criterion, epoch, RESUME)
 
 
 # hook the feature extractor
@@ -101,11 +102,15 @@ features_blobs = []
 def hook_feature(module, input, output):
     features_blobs.append(output.data.cpu().numpy())
 
-net._modules.get(final_conv).register_forward_hook(hook_feature)
+
+final_conv = 'Mixed_7c'
+model._modules.get(final_conv).register_forward_hook(hook_feature)
 
 
 # CAM
 if CAM:
     root = 'sample.jpg'
     img = Image.open(root)
-    get_cam(net, features_blobs, img, classes, root)
+    # class
+    classes = {0: 'cat', 1: 'dog'}
+    get_cam(model, features_blobs, img, classes, root)
--- pytorch_CAM/train.py
+++ pytorch_CAM/train.py
@@ -1,42 +1,43 @@
 import os
 
 import torch
-from torch.autograd import Variable
 
 
-def retrain(trainloader, model, use_cuda, epoch, criterion, optimizer):
+def retrain(trainloader, model, device, epoch, criterion, optimizer):
     model.train()
-    correct, total = 0, 0
-    acc_sum, loss_sum = 0, 0
-    i = 0
-    for batch_idx, (data, target) in enumerate(trainloader):
-        if use_cuda:
-            data, target = data.cuda(), target.cuda()
-        data, target = Variable(data), Variable(target)
+
+    acc_sum = 0
+    loss_sum = 0
+
+    for batch_idx, (image, label) in enumerate(trainloader):
+        image, label = image.to(device), label.to(device)
+
+        prediction = model(image)
+        loss = criterion(prediction, label)
+
         optimizer.zero_grad()
-        output = model(data)
-
-        # calculate accuracy
-        correct += (torch.max(output, 1)[1].view(target.size()).data == target.data).sum()
-        total += trainloader.batch_size
-        train_acc = 100. * correct / total
-        acc_sum += train_acc
-        i += 1
-
-        loss = criterion(output, target)
         loss.backward()
         optimizer.step()
+
         loss_sum += loss.item()
+
+        # calculate accuracy
+        correct = (torch.max(prediction, 1)[1].view(label.size()).data == label.data).sum()
+        total = trainloader.batch_size
+        train_acc = 100. * correct / total
+        acc_sum += train_acc
 
         if batch_idx % 10 == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.3f}\tTraining Accuracy: {:.3f}%'.format(
-                epoch, batch_idx * len(data), len(trainloader.dataset),
+                epoch, batch_idx * len(image), len(trainloader),
                 100. * batch_idx / len(trainloader), loss.item(), train_acc))
 
-    acc_avg = acc_sum / i
-    loss_avg = loss_sum / len(trainloader.dataset)
-    print()
-    print('Train Epoch: {}\tAverage Loss: {:.3f}\tAverage Accuracy: {:.3f}%'.format(epoch, loss_avg, acc_avg))
+    acc_avg = acc_sum / len(trainloader)
+    loss_avg = loss_sum / len(trainloader)
+
+    result = '\nTrain Epoch: {}\tAverage Loss: {:.3f}\tAverage Accuracy: {:.3f}%'.format(
+        epoch, loss_avg, acc_avg)
+    print(result)
 
     os.makedirs('result', exist_ok=True)
     with open('result/train_acc.txt', 'a') as f:
@@ -46,25 +47,32 @@ def retrain(trainloader, model, use_cuda, epoch, criterion, optimizer):
         f.write(str(loss_avg))
     f.close()
 
-def retest(testloader, model, use_cuda, criterion, epoch, RESUME):
-    model.eval()
-    test_loss = 0
-    correct = 0
-    for data, target in testloader:
-        if use_cuda:
-            data, target = data.cuda(), target.cuda()
-        data, target = Variable(data, volatile=True), Variable(target)
-        output = model(data)
-        # sum up batch loss
-        test_loss += criterion(output, target).item()
-        # get the index of the max log-probability
-        pred = output.data.max(1, keepdim=True)[1]
-        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
 
-    test_loss /= len(testloader.dataset)
-    test_acc = 100. * correct / len(testloader.dataset)
+def retest(testloader, model, device, criterion, epoch, RESUME):
+    model.eval()
+
+    loss_sum = 0
+    correct_sum = 0
+
+    for image, label in testloader:
+        image, label = image.to(device), label.to(device)
+
+        prediction = model(image)
+
+        # sum up batch loss
+        loss = criterion(prediction, label)
+        loss_sum += loss.item()
+
+        # get the index of the max log-probability
+        pred = prediction.data.max(1, keepdim=True)[1]
+        correct = pred.eq(label.data.view_as(pred)).cpu().sum()
+        correct_sum += correct
+
+    test_loss = loss_sum / len(testloader.dataset)
+    test_acc = 100. * correct_sum / len(testloader.dataset)
+
     result = '\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.3f}%)\n'.format(
-        test_loss, correct, len(testloader.dataset), test_acc)
+        loss_sum, correct_sum, len(testloader.dataset), test_acc)
     print(result)
 
     # Save checkpoint.
